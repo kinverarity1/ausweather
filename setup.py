a20c38793872629b8cb34339125323606bc3199b
Kent Inverarity <kinverarity@hotmail.com>
2020-02-11 19:23:30 +1030

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="ausweather",
+    packages=["ausweather"],
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="Access historical weather data in Australia",
+    long_description=open("README.md", mode="r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kinverarity1/ausweather",
+    author="Kent Inverarity",
+    author_email="kinverarity@hotmail.com",
+    license="MIT",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+    ),
+    keywords="science",
+    install_requires=("pandas", "requests"),
+    include_package_data=True,
+)
